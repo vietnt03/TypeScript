e7e7d939e041f812b4141d93e79973cb75696d47
vietnt03 <viet2000333@gmail.com>
2023-03-15 17:07:14 +0700

    update_lap1

--- less/dist/datatypes/Genderic.js
+++ less/dist/datatypes/Genderic.js
@@ -1,34 +1,5 @@
-// const arrNum  = [1, 2, 3, 4, 5]
-// const arrStr = ["a","b","c","d"] 
-// function ohop_map <T> (arr : T[], callback : (item :T)=>T) : T[] {
-//     let temp = []
-//     for (let i = 0; i < arr.length; i++) {
-//         const newohop = callback (arr[i]);
-//         temp.push(newohop)
-//     }
-//     return temp
-// }
-// const result = ohop_map(arrNum, (item) => {
-//     return item * item
-// })
-// console.log(result);
-var arrNum = [100, 12, 3, 5];
-var arrStr = ["obama", "iphone", "abc"];
-//   function selectionSort <T> (arr : T[], callback : (a :T ,b :T)=>number) : T[] {
-//     const len = arr.length;
-//     for (let i = 0; i < len - 1; i++) {
-//       let minIndex = i;
-//       for (let j = i + 1; j < len; j++) {
-//         if (callback(arr[j], arr[minIndex]) < 0) {
-//           minIndex = j;
-//         }
-//       }
-//       [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-//     }
-//     return arr;
-//   }
-//   console.log(selectionSort(arrStr ,(a,b)=> a-b)); 
-// console.log(selectionSort(arrStr, (a, b) => a.localeCompare(b)));
+var arrNum1 = [100, 12, 3, 5];
+var arrStr1 = ["abc", "iphone", "abc"];
 function selectionSort(arr, callback) {
     var _a;
     var len = arr.length;
@@ -41,5 +12,11 @@ function selectionSort(arr, callback) {
     }
     return arr;
 }
-console.log(selectionSort(arrStr, function (a, b) { return a.localeCompare(b); }));
-//   console.log(selectionSort(arrStr ,(a,b)=> a-b)); 
+function ascendingOrder(a, b) {
+    return a - b;
+}
+selectionSort(arrNum1, ascendingOrder);
+console.log(arrNum1);
+arrStr1.sort();
+arrStr1.sort().reverse();
+console.log(arrStr1);
--- less/src/datatypes/Genderic.ts
+++ less/src/datatypes/Genderic.ts
@@ -1,57 +1,38 @@
-// const arrNum  = [1, 2, 3, 4, 5]
-// const arrStr = ["a","b","c","d"] 
 
-// function ohop_map <T> (arr : T[], callback : (item :T)=>T) : T[] {
-//     let temp = []
-//     for (let i = 0; i < arr.length; i++) {
-//         const newohop = callback (arr[i]);
-//         temp.push(newohop)
-        
-//     }
-//     return temp
-// }
-// const result = ohop_map(arrNum, (item) => {
-//     return item * item
-// })
-// console.log(result);
 
-const arrNum = [100,12,3,5] 
-const arrStr = ["obama","iphone","abc"];
+const arrNum1 = [100,12,3,5] 
+const arrStr1 = ["abc","iphone","abc"];
 
-//   function selectionSort <T> (arr : T[], callback : (a :T ,b :T)=>number) : T[] {
-//     const len = arr.length;
-  
-//     for (let i = 0; i < len - 1; i++) {
-//       let minIndex = i;
-  
-//       for (let j = i + 1; j < len; j++) {
-//         if (callback(arr[j], arr[minIndex]) < 0) {
-//           minIndex = j;
-//         }
-//       }
-  
-//       [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-//     }
-  
-//     return arr;
-//   }
-    //   console.log(selectionSort(arrStr ,(a,b)=> a-b)); 
-    // console.log(selectionSort(arrStr, (a, b) => a.localeCompare(b)));
-function selectionSort<T>(arr: T[], callback: (item: T, item2: T) => number): T[] {
+
+function selectionSort<T>(arr: T[], callback: (a: T, b: T) => number): T[] {
     const len = arr.length;
     
     for (let i = 0; i < len - 1; i++) {  
       for (let j = i + 1; j < len; j++) { 
         if(callback(arr[j], arr[i]) < 0) {
           [arr[j], arr[i]] = [arr[i], arr[j]];
+         
         }
+
       }
+      
     }
     return arr;
   }
-  console.log(selectionSort(arrStr, (a, b) => a.localeCompare(b)));
-    //   console.log(selectionSort(arrStr ,(a,b)=> a-b)); 
+  function ascendingOrder(a: number, b: number): number {
+    return a - b;
+  }
   
+  selectionSort(arrNum1, ascendingOrder);
+  console.log(arrNum1);
+  arrStr1.sort();
+  
+  arrStr1.sort().reverse();
+  console.log(arrStr1);
+  
+  
+  
+
 
 
 
